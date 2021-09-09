15a4e0724c981647428d5e9b210e6f34b97f407e
mercy ekoh <nanya@mercys-MacBook-Pro.local>
2021-09-09 15:13:41 +0100

    initial commit

---
+++ second_class.py
@@ -0,0 +1,51 @@
+# welcome = 5
+
+# # print(welcome)
+
+# Gender = 'Male'
+# Population = 1000000
+# Girlisfire = True
+# Minus = 0.554
+
+# # print(type(Gender))
+
+# my_list = [True, 'A boy', 95,['Mercy', 0.996, 'John']]
+# print(type(my_list))
+
+
+# a = 3
+# b = 6
+# c = 3
+# x = -b + (b**2 - 4*a*c)**0.5
+# ans = x / (2*a)
+# print(ans)
+
+# a = 5**0.5
+# b = a + 2
+# c = 15**2
+# x = b/c
+# print(x)
+
+# a=2
+# b=3
+# print (~(a is b))
+
+# first = (1 + (0.08/12))
+# second = 10000 * (first ** 60)
+# print(second)
+
+# # a = 4; b = 9; c = 8
+# s = a + b + c
+# # P= (s-a)*(s-b)*(s-c)
+# # area = (s*P)**0.5
+# # print(area)
+
+# A=57
+# B= A//2
+# print(B)
+
+
+# Income = 100000
+# monthly = (0.1*100000)
+# yearly = (12 * monthly)
+# print(yearly)
