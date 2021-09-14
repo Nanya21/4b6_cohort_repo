451d841b70c7170ee3952ffcb3fdf8183f46417a
mercy ekoh <ekohifunaya@gmail.com>
2021-09-14 19:22:21 +0100

    secondweek_assignment

--- Secondweek_assignment.py
+++ Secondweek_assignment.py
@@ -1,18 +1,20 @@
 s1="Ault"; s2='Kelly'
 first_slice= (s1[0:2])
 second_slice= (s1[2:])
-print(first_slice+s2+second_slice)
+output =(first_slice+s2+second_slice)
+print(output)
 
 
 p1='America'; p2= 'Japan'
 slicep1= (p1 [0:8:3])
 slicep2= (p2 [0:5:2])
 fullslice=slicep1+slicep2
-print(fullslice[0]+fullslice[3]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[5])
+finaloutput=(fullslice[0]+fullslice[3]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[5])
+print(finaloutput)
+
 
 b1='ABC'; b2='XYZ'
 fullslice=b1+b2
-print(fullslice[0]+fullslice[5]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[3])
-
-
+third_string=(fullslice[0]+fullslice[5]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[3])
+print(third_string)
 
