706311dd7b9b05a4d32629a6dceb3b962cb6c07d
mercy ekoh <ekohifunaya@gmail.com>
2021-09-16 16:03:39 +0100

    List methods

---
+++ List.py
@@ -0,0 +1,91 @@
+# a=[12,13,20,20,1,10,15, 22,14]
+# a[3]=25
+# print(a)
+
+# a = [3,6,7]; b=[8,9,10]
+# c= a+b
+# print(c)
+
+# a= [8,7,2,4]
+# b= [4,2,9,0]
+# a.extend(b)
+# print(a)
+# b.append(10)
+# print(b)
+
+names = ['nankoh', 'marane_10', ]
+
+# user= input ('Hi, what is your last name: ')
+# user2=input('Your first name: ')
+# usert= (user[0:3])
+# name=(user2[-2:])
+# username=(usert+name)
+# # print(username)
+# print('Hello, {}. \n Thank you for signing up. \nYour account has successfully been added.\nYour account id is {}.\n\tCheers.\n\tAdmin'.format(user2.title(),username))
+# names.append(username)
+# print(names)
+
+amount= [500,200,[200,500,700,[250,800],250],[1000]]
+# amt= amount[2]
+# amt1= amt[2]
+# print(amt1)
+# amot= amt[3]
+# amot1=amot[1]
+# print(amot1)
+# amt2=amot1+amt1
+# amount[3].append(amt2)
+# print(amount)
+
+
+
+
+# str1='Emma is a data scientist who knows python. Emma works at google.'
+# ind= str1.rindex('Emma')
+# print(ind)
+
+artiste=['nicki','justin','rihanna','bey']
+music=artiste
+song=artiste.copy()
+# artiste[1]='kanye'
+# print(artiste)
+# print(music)
+# print(song)
+# artiste.remove('justin')
+print(artiste)
+music.pop(0)
+popelement= music.pop(0)
+print(music)
+print(popelement)
+# artiste.insert(2, 'jcole')
+# print(artiste)
+# artiste.sort()
+# print(artiste)
+# artiste.reverse()
+# print(artiste)
+
+# print(type(range(5)))
+# myfunc()
+
+
+# a='PYnative'
+# print(a[1:4], a[:5], a[4:],a[0:-1], a[:-1])
+
+# a='welcome'
+# print(a*2)
+
+# b="my isname isisis jameis isis bond"
+# a='is'
+# print(b.count(a,4))
+
+# a='pynative'
+# b= ['abc', 'pynative', 'xyz']
+
+# print(b[1]== a)
+# print(b[1] is a)
+
+# a=[None]*10
+# print(len(a))
+
+# b=[5,10,15,25]
+# print(b[::-2])
+
--- Secondweek_assignment.py
+++ Secondweek_assignment.py
@@ -5,16 +5,26 @@ output =(first_slice+s2+second_slice)
 print(output)
 
 
-p1='America'; p2= 'Japan'
-slicep1= (p1 [0:8:3])
-slicep2= (p2 [0:5:2])
-fullslice=slicep1+slicep2
-finaloutput=(fullslice[0]+fullslice[3]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[5])
-print(finaloutput)
+# p1='America'; p2= 'Japan'
+# slicep1= (p1 [0:8:3])
+# slicep2= (p2 [0:5:2])
+# fullslice=slicep1+slicep2
+# finaloutput=(fullslice[0]+fullslice[3]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[5])
+# print(finaloutput)
 
 
-b1='ABC'; b2='XYZ'
-fullslice=b1+b2
-third_string=(fullslice[0]+fullslice[5]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[3])
-print(third_string)
+# b1='ABC'; b2='XYZ'
+# fullslice=b1+b2
+# third_string=(fullslice[0]+fullslice[5]+fullslice[1]+fullslice[4]+fullslice[2]+fullslice[3])
+# print(third_string)
 
+
+p1='America' ; p2 = 'Japan'
+a= p1[0]
+b= p2[0]
+c= p1[len(p1)//2]
+d= p2[len(p2)//2]
+e= p1[-1]
+f=p2[-1]
+solution= (a+b+c+d+e+f)
+print(solution)
